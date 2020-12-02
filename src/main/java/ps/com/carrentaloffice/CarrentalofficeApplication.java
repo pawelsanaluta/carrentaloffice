590cbd4153afcf4941d2a13096f94032d3c0a734
Pawel Sanaluta <pawel.sanaluta@gmail.com>
2020-12-02 18:44:26 +0100

    initial commit

---
+++ src/main/java/ps/com/carrentaloffice/CarrentalofficeApplication.java
@@ -0,0 +1,13 @@
+package ps.com.carrentaloffice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarrentalofficeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CarrentalofficeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ps/com/carrentaloffice/CarrentalofficeApplicationTests.java
@@ -0,0 +1,13 @@
+package ps.com.carrentaloffice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarrentalofficeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
